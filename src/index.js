1b1a36c9522206ab886d3f6b07d01549c8ce0bfe
INFINIT3I <38301254+Infinit3i@users.noreply.github.com>
2024-05-09 23:59:51 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,200 +9,130 @@ const client = new Client({
     IntentsBitField.Flags.MessageContent,
   ],
 });
-// [question, answer, hint, blank]
+
+//Question Bank
+/*
+  {
+    category: '',
+    question: '',
+    answer: '',
+    hint: '',
+    blank: '',
+    review: '',
+  },
+*/
 let questions = [
-  [
-    "What is a botnet?",
-    "network of compromised devices",
-    "Controlled by attackers",
-    "xxxxxxxx xx xxxxxxxxxxxxxx xxxxxxx",
-  ],
-  [
-    "What is a cybersecurity awareness?",
-    "security education",
-    "Promoting security knowledge",
-    "xxxxxxxx xxxxxxxxx",
-  ],
-  [
-    "What is a data breach?",
-    "information exposure",
-    "Unauthorized data access",
-    "xxxxxxxxxxxxxxxx xxxxxxxxx",
-  ],
-  [
-    "What is a denial-of-service attack?",
-    "service disruption",
-    "Overloads servers or networks",
-    "xxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxx",
-  ],
-  [
-    "What is a password manager?",
-    "secure password storage",
-    "Organizes and encrypts passwords",
-    "xxxxxxxx xxxxxxxx xxxxxxxx",
-  ],
-  [
-    "What is a phishing attack?",
-    "fraudulent email scheme",
-    "Deceptive email campaigns",
-    "xxxxxxxxx xxxx xxxxxxxxx xxxxxxxx",
-  ],
-  ["What is a phishing?", "email scam", "What type of scam?", "xxxxx xxxx"],
-  ["What is a rootkit?", "hidden malware", "invisible", "xxxxxx xxxxxxx"],
-  [
-    "What is a security incident?",
-    "security breach event",
-    "Violates security policies",
-    "xxxxxxxx xxxxxx xxxxxx",
-  ],
-  [
-    "What is a vulnerability assessment?",
-    "security check",
-    "Identifying weaknesses",
-    "xxxxxxxxxx xxxxx",
-  ],
-  [
-    "What is a vulnerability scanner?",
-    "security tool",
-    "Identifies system weaknesses",
-    "xxxxxxxxxxxx xxxxx",
-  ],
-  [
-    "What is a worm?",
-    "self-replicating malware",
-    "Replicates independently",
-    "xxxxxxx xxxxxxxxxx xxxxxxxx",
-  ],
-  [
-    "What is an intrusion detection system?",
-    "network monitoring tool",
-    "Alerts on suspicious activities",
-    "xxxxxxxx xxxxxxxxx xxxxx",
-  ],
-  [
-    "What is biometric authentication?",
-    "unique biological identifiers",
-    "Fingerprints or iris scans",
-    "xxxxxxxx xxxxxxxxx xxxxxxxxxxxxxxxxxx",
-  ],
-  [
-    "What is cyber hygiene?",
-    "security best practices",
-    "Maintaining good security habits",
-    "xxxxxxxxx xxxxx xxxxxxxxx",
-  ],
-  [
-    "What is digital forensics?",
-    "evidence analysis",
-    "Investigating cyber incidents",
-    "xxxxxxxxxxxx xxxxxxxxxxx",
-  ],
-  ["What is encryption?", "data protection", "", ""],
-  [
-    "What is malware analysis?",
-    "malicious code examination",
-    "Studying harmful programs",
-    "xxxxxxxxxx xxxxxxxxxxxxxx",
-  ],
-  [
-    "What is malware?",
-    "malicious software",
-    "What type of software?",
-    "xxxxxxxxx xxxxxxxx",
-  ],
-  [
-    "What is network segmentation?",
-    "network isolation",
-    "Dividing network into parts",
-    "xxxxxxxxxxxxxxxx xxxxxxx",
-  ],
-  [
-    "What is secure coding?",
-    "secure software development",
-    "Writing safe programs",
-    "xxxxxxxx xxxxxxxx xxxxxxxxxxxxxxxx",
-  ],
-  ["What is social engineering?", "manipulative tactics", "", ""],
-  [
-    "What is social media engineering?",
-    "manipulative online tactics",
-    "Deceptive online strategies",
-    "xxxxxxxxxxxx xxxxxxxxxx",
-  ],
-  [
-    "What is the OSI model?",
-    "open systems interconnection",
-    "CMON!?",
-    "xxxx xxxxxxx xxxxxxxxxxxxxxx",
-  ],
-  ["What is two-factor authentication (2FA)?", "extra verification", "", ""],
-  [
-    "What is zero-day vulnerability?",
-    "unknown exploit",
-    "Unknown and unaddressed flaw",
-    "xxxxxxx xxxxxxx",
-  ],
-  [
-    "What is vlan?",
-    "segments networks",
-    "what does it do to networks?",
-    "xxxxxxx xxxxxxxx",
-  ],
+  {
+    category: 'GCIH',
+    question: 'What application uses hook.js to attack victims from the browser?',
+    answer: 'BeEF',
+    hint: 'MOOOOO',
+    blank: 'xxxx',
+    review: 'https://youtu.be/etqw6MpisYw?si=c2ae_m0xywwBGZan'
+  },
+  {
+    category: 'General',
+    question: 'What is the OSI model?',
+    answer: 'Open Systems Interconnection',
+    hint: 'https://aws.amazon.com/what-is/osi-model/',
+    blank: 'xxxx xxxxxxx xxxxxxxxxxxxxxx',
+    review: 'https://youtu.be/Ilk7UXzV_Qc?si=eFvm1JMCByP6iwJ1',
+  },
+  {
+    category: 'General',
+    question: 'What do vlans do?',
+    answer: 'logically segment',
+    hint: 'physical vs logical does what?',
+    blank: 'xxxxxxxxx xxxxxxx',
+    review: 'https://www.google.com',
+  },
+  {
+    category: 'General',
+    question: 'What is Phishing?',
+    answer: 'Email scam',
+    hint: 'What is it sent over?',
+    blank: 'xxxxx xxxx',
+    review: 'https://www.google.com',
+  },
+  {
+    category: 'General',
+    question: 'What is a DOS attack',
+    answer: 'Denial of service',
+    hint: 'What does it stop?',
+    blank: 'xxxxxx xx xxxxxxx',
+    review: 'https://www.google.com',
+  },
+  {
+    category: 'General',
+    question: 'What is a botnet?',
+    answer: 'Many compromised devices',
+    hint: 'Controlled by attackers',
+    blank: 'xxxx xxxxxxxxxxxxxx xxxxxxx',
+    review: 'https://www.google.com',
+  },
+  {
+    category: 'General',
+    question: 'What is a botnet?',
+    answer: 'Many compromised devices',
+    hint: 'Controlled by attackers',
+    blank: 'xxxx xxxxxxxxxxxxxx xxxxxxx',
+    review: 'https://www.google.com',
+  },
 ];
 
-function getRandomQuestion(questions) {
-  const randomIndex = Math.floor(Math.random() * questions.length);
-  return questions[randomIndex];
+function generatenew() {
+  let keys = Object.keys(questions);
+  let numofkeys = keys.length;
+  return randomNumber = Math.floor(numofkeys * Math.random())
 }
 
-const randomQuestion = getRandomQuestion(questions);
-const channelID = "1107760560008876214";
+let score = 0;
+const channelID = process.env.CQOTD_ID;
 let askingQuestion = false; // Flag to track if a question is being asked
 
 client.on("ready", (c) => {
   console.log(`✅ ${c.user.tag} is online.`);
 });
 
-client.on("messageCreate", (msg) => {
+//cqotd
+client.on('messageCreate', (msg) => {
   console.log(`${msg.author.displayName} said ${msg.content}`);
-  if (msg.author.bot) {
+  if (msg.author.bot || msg.channel.id !== channelID) {
     return;
-  }
-
-  if (
-    msg.content === "cqotd" &&
-    !msg.author.bot &&
-    msg.channel.id == channelID
+  } else if (
+    msg.content.toLowerCase() === "cqotd"
   ) {
-    msg.reply(randomQuestion[0]);
+    let randomNumber = generatenew();
+    msg.reply(questions[randomNumber].question);
     askingQuestion = true;
   } else if (
-    msg.content === "help" &&
-    !msg.author.bot &&
-    msg.channel.id == channelID
-  ) {
-    msg.reply(
-      "I can help you with Cyber security questions. I was made by Infinit3i to send out cyber security questions whenever you are bored. \n cqotd - (cyber question of the day). \n help - Help prompt. \n hint -   Current questions hint. \n question - What is the current question. \n blank - Blank spaces xxxxxx."
-    );
-  } else if (!askingQuestion || msg.channel.id !== channelID) {
-    return;
-  } else if (
-    msg.content.toLowerCase() === randomQuestion[1] &&
+    msg.content.toLowerCase() === questions[randomNumber].answer.toLowerCase() &&
     askingQuestion
   ) {
-    msg.reply(`✅ You got it right! 🎉`);
+    msg.reply(`✅ You got it right! 🎉 \n \n ${questions[randomNumber].review}`);
+    msg.reply(`Your Score is ${score = score + 5}`);
     askingQuestion = false;
-  } else if (msg.content === "question" && !msg.author.bot && askingQuestion) {
-    msg.reply(randomQuestion[0]);
-  } else if (msg.content === "hint" && !msg.author.bot && askingQuestion) {
-    msg.reply(`They hint is ${randomQuestion[2]}`);
-  } else if (msg.content === "blank" && !msg.author.bot && askingQuestion) {
-    msg.reply(`The blanks are ${randomQuestion[3]}`);
   } else {
     msg.reply(`❌ You got it WRONG!`);
   }
 });
 
+//commands for the Cyber Nerds Bot
+client.on('interactionCreate', (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "question") {
+    interaction.reply({content: questions[randomNumber].question, ephemeral: true});
+  } else if (interaction.commandName === "hint") {
+    interaction.reply({ content: `They hint is ${questions[randomNumber].hint}`, ephemeral: true});
+  } else if (interaction.commandName === "blank") {
+    interaction.reply({ content: `The blanks are ${questions[randomNumber].blank}`, ephemeral: true});
+  } else if (interaction.commandName === "score") {
+    interaction.reply({ content: `Your score is ${score}`, ephemeral: true});
+  }
+})
+
+//registering users to roles
 client.on("interactionCreate", async (interaction) => {
   try {
     if (!interaction.isButton()) return;
@@ -231,4 +161,18 @@ client.on("interactionCreate", async (interaction) => {
   }
 });
 
+//MongoDB
+(async () => {
+  try {
+    mongoose.set('strictQuery', false)
+    await mongoose.connect(process.env.MONGODB_URI, { keepalive: true});
+    console.log('Connected to DB.');
+    eventHandler(client);
+    
+  } catch (error) {
+    console.log(`Error ${error}`);
+  }
+})
+
 client.login(process.env.TOKEN);
+
