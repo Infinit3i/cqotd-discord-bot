9fda5df1490f9632967abbe4cbd631a376e23c65
Matthew Iverson <38301254+Matthew-Iverson@users.noreply.github.com>
2024-05-10 15:25:23 -0400

    Update score.js
    logic for score is done now i need to create user model

--- src/score.js
+++ src/score.js
@@ -1,32 +1,53 @@
-const { Client, Interaction, ApplicationcommandOptionType } = require('discord.js');
-const User = require('../models/User')
+const {
+  Client,
+  Interaction,
+  ApplicationcommandOptionType,
+} = require("discord.js");
+const User = require("../models/User");
 
 module.exports = {
-        /**
-         * 
-         * @param {Client} client 
-         * @param {Interaction} interaciton
-         **/
-        callback: async (client, interaction) => {
-            if (!interaction.inGuild()) {
-                interaction.reply({
-                    content: 'You can only run this command inside of a server.',
-                    ephermeral: true,
-                });
-                return;
-            }
-            const targetUserId = interaction.options.get('user')?.value || interaction.member.id;
+  /**
+   *
+   * @param {Client} client
+   * @param {Interaction} interaciton
+   **/
+  callback: async (client, interaction) => {
+    if (!interaction.inGuild()) {
+      interaction.reply({
+        content: "You can only run this command inside of a server.",
+        ephermeral: true,
+      });
+      return;
+    }
+    const targetUserId =
+      interaction.options.get("user")?.value || interaction.member.id;
 
-            await interaction.deferReply();
-        },
+    await interaction.deferReply();
 
-    name: 'score',
-    description: "See someones score.",
-    options: [
-        {
-            name: 'user',
-            description: 'Someones score.',
-            type: ApplicationcommandOptionType.User,
-        }
-    ]
-}
+    const user = await User.findOne({
+      userId: targetUserId,
+      guildId: interaction.guild.id,
+    });
+
+    if (!user) {
+      interaction.editReply(`<@${targetUserId}> doesn't have a profile yet.`);
+      return;
+    }
+
+    interaction.editReply(
+      targetUserId === interaction.member.id
+        ? `Your score is **${user.score}**`
+        : `<@${targetUserId}>'s score is **${user.score}**`
+    );
+  },
+
+  name: "score",
+  description: "See someones score.",
+  options: [
+    {
+      name: "user",
+      description: "Someones score.",
+      type: ApplicationcommandOptionType.User,
+    },
+  ],
+};
