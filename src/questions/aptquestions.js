56da050930b7ad390e23e3c7eb9a65936adfa886
infinit3i <matthewiver@protonmail.com>
2024-12-22 22:03:01 -0500

    more questions

--- src/questions/aptquestions.js
+++ src/questions/aptquestions.js
@@ -181,7 +181,7 @@ let aptquestions = [
       answer: "SWIFT",
       hint: "Targets banking systems.",
       blank: "xxxxx",
-      review: "https://www.fireeye.com/current-threats/apt-groups/lazarus.html",
+      review: "https://community.broadcom.com/symantecenterprise/communities/community-home/librarydocuments/viewdocument?DocumentKey=8ae1ff71-e440-4b79-9943-199d0adb43fc&CommunityKey=1ecf5f55-9545-44d6-b0f4-4e4a7f5f5e68&tab=librarydocuments",
     },
     {
       category: "APTs",
